7cf8de660472881c3f1ebf792533245d553446cb
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-11 08:45:55 +0500

    Javascript assignment
    Javascript assignment

---
+++ app.js
@@ -0,0 +1,70 @@
+// Question no 01:
+// var userInput  = prompt("Enter a positive integer");
+// var number = userInput;
+// console.log("number" + " " + number);
+
+// var roundOffValue = Math.round(number);
+// console.log("Round off value is" + " " + roundOffValue);
+
+// var floorValue = Math.floor(number);
+// console.log("Floor value is" + " " + floorValue);
+
+// var ceilValue = Math.ceil(number);
+// console.log("Ceil value is" + " " + ceilValue);
+
+
+
+// Question no 02:
+// var userInput  = prompt("Enter a Negative floating number");
+// var number = userInput;
+// console.log("number" + " " + number);
+
+// var roundOffValue = Math.round(number);
+// console.log("Round off value is" + " " + roundOffValue);
+
+// var floorValue = Math.floor(number);
+// console.log("Floor value is" + " " + floorValue);
+
+// var ceilValue = Math.ceil(number);
+// console.log("Ceil value is" + " " + ceilValue);
+
+
+// let num = Math.random() * 2 + 1
+
+// console.log(num.toFixed());
+
+
+// Question no 03:
+// var number = prompt("enter your num");
+// var num = -4;
+
+// console.log("The Absolute value of" + num + " is " + number); 
+
+
+// var number = prompt("Enter a Number");
+// num = 5;
+// console.log("The Absolute value of " + num + " is " + number);
+
+
+// Question no 04:
+// let num = Math.random() * 4
+// let num1 = Math.random() * 6
+
+// console.log(num.toFixed());
+// console.log(num1.toFixed());
+
+//  Question no 05:
+
+// var  coin = Math.random();
+// if(coin < 2){
+//     console.log("Heads");
+//     }
+// else{
+// console.log("Tails");
+// }
+        
+
+
+// Question no 06:
+// var number;
+// document.write(Math.random(number) * 100);
